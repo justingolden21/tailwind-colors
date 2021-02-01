125873de8c06bdbb7c334bcc16f0d0bb752138c4
Justin Golden <justingolden21@gmail.com>
2021-01-31 17:57:35 -0800

    css updates
    updates for @media print and checkmark on color when copied

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,13 +5,13 @@ window.onload = ()=> {
 		for(let key in colors[color]) {
 			if(key=='name') continue;
 			let hex = colors[color][key];
-			html += `<div class="color"><p class="number">${key}</p><div class="color-preview" style="background-color:${hex};" onclick="copyText('${hex}',this)"></div><p class="hex">${hex}</p></div>`;
+			html += `<div class="color"><p class="number">${key}</p><div class="color-preview" style="background-color:${hex};" onclick="copyText('${hex}',${key},this)"></div><p class="hex">${hex}</p></div>`;
 		}
 		html += '</div>';
 	}
 	document.getElementById('colors').innerHTML = html;
 };
-function copyText(text,elm) {
+function copyText(text,num,elm) {
 	let input = document.createElement('input');
 	input.setAttribute('value', text);
 	document.body.appendChild(input);
@@ -20,8 +20,7 @@ function copyText(text,elm) {
 	document.body.removeChild(input);
 
 	const check = `<svg style="position:relative;top:8px;left:8px;width:16px;height:16px;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-	<circle r="12" cx="12" cy="12" fill="#F9FAFB"></circle>
-	<path stroke="#374151" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
+	<path stroke="#${num>300?'F9FAFB':'374151'}" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M5 13l4 4L19 7"></path>
 	</svg>`;
 	elm.innerHTML = check;
 	setTimeout(()=> elm.innerHTML = '', 750);
